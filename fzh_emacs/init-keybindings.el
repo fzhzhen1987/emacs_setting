bc9617616a1723429e61a40e253aa248b35a904f
fzhzhen1987 <fzhzhen1987@yahoo.co.jp>
2019-03-03 22:29:04 +0900

    change keybind of search

--- fzh_emacs/init-keybindings.el
+++ fzh_emacs/init-keybindings.el
@@ -19,7 +19,7 @@
 
 
 ;;swiper
-(global-set-key "\C-s" 'swiper)
+;;(global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 ;;(global-set-key (kbd "M-x") 'counsel-M-x)
 ;;(global-set-key (kbd "C-x C-f") 'counsel-find-file)
